72345d0824fb36e886a436255283fa1cbdeb5273
marcromanos <64471489+marcromanos@users.noreply.github.com>
2022-01-24 20:16:13 +0100

    getters setters strings construcotrs

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -4,4 +4,24 @@ public class Client extends Persona{
 
 	private String correu;
 	
+	public Client(String nom, String cognom, String adreça, String dni, int telefon, String correu) {
+		super(nom, cognom, adreça, dni, telefon);
+		this.correu = correu;
+	}
+
+	public String getCorreu() {
+		return correu;
+	}
+
+	public void setCorreu(String correu) {
+		this.correu = correu;
+	}
+
+	@Override
+	public String toString() {
+		return "Client [correu=" + correu + "]";
+	}
+	
+	
+	
 }
--- src/main/java/model/Reserva.java
+++ src/main/java/model/Reserva.java
@@ -11,5 +11,88 @@ public class Reserva {
 	private Torn torn;
 	private int comensals;
 	private String observacions;
+
+	public Reserva(int idReserva, Client client, Restaurant restaurant, LocalDate data, Torn torn, int comensals,
+		String observacions) {
+		this.idReserva = idReserva;
+		this.client = client;
+		this.restaurant = restaurant;
+		this.data = data;
+		this.torn = torn;
+		this.comensals = comensals;
+		this.observacions = observacions;
+	}
+	
+	public Reserva(Client client, Restaurant restaurant, LocalDate data, Torn torn, int comensals,
+		String observacions) {
+		this.client = client;
+		this.restaurant = restaurant;
+		this.data = data;
+		this.torn = torn;
+		this.comensals = comensals;
+		this.observacions = observacions;
+	}
+
+	public int getIdReserva() {
+		return idReserva;
+	}
+
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public Restaurant getRestaurant() {
+		return restaurant;
+	}
+
+	public void setRestaurant(Restaurant restaurant) {
+		this.restaurant = restaurant;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public Torn getTorn() {
+		return torn;
+	}
+
+	public void setTorn(Torn torn) {
+		this.torn = torn;
+	}
+
+	public int getComensals() {
+		return comensals;
+	}
+
+	public void setComensals(int comensals) {
+		this.comensals = comensals;
+	}
+
+	public String getObservacions() {
+		return observacions;
+	}
+
+	public void setObservacions(String observacions) {
+		this.observacions = observacions;
+	}
+
+	@Override
+	public String toString() {
+		return "Reserva [idReserva=" + idReserva + ", client=" + client + ", restaurant=" + restaurant + ", data="
+				+ data + ", torn=" + torn + ", comensals=" + comensals + ", observacions=" + observacions + "]";
+	}
+	
+	
+	
+	
 	
 }
