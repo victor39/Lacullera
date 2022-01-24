35f5d8796c97a53dd110cc5b04419c56ee43386a
victor <valarcon@gmail.com>
2022-01-24 18:59:18 +0100

    primer commit

---
+++ src/main/java/dam2/amv/App.java
@@ -0,0 +1,13 @@
+package dam2.amv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
