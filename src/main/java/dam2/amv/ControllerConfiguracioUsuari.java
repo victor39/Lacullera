72f8c4e0b9eb758a07485e8eb96784137cdea6a1
marcromanos <64471489+marcromanos@users.noreply.github.com>
2022-04-04 20:10:58 +0200

    h

--- src/main/java/dam2/amv/ControllerConfiguracioUsuari.java
+++ src/main/java/dam2/amv/ControllerConfiguracioUsuari.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import model.Client;
 import model.ClientDAOImpl;
 import model.Connexio;
 
@@ -44,11 +45,13 @@ public class ControllerConfiguracioUsuari {
 
 		ClientDAOImpl client = new ClientDAOImpl();
 		
+		Client cliente = new Client(TFNom,TFCognom,TFA);
+		
     	String id = TFid.getText();
     	
     	System.out.println("id agafat correctament: " + id);
     	
-    	
+    	client.Tots(con, client);
     	
     }
 
